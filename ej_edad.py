8c63a7f6116edb47ead88ac0aa9da54f49022599
sgbm0592 <sgbm0592@gmail.com>
2021-03-02 19:33:18 -0600

    Revert "ejercicios"
    This reverts commit 83d2f0a09d0845f779321b9dfe3d5cd150bb777b.

--- ej_edad.py
+++ ej_edad.py
@@ -1,28 +1,28 @@
 import datetime
 mes = 6
 dia = 5
-año = 1992
+year = 1992
 
-fecha_nacimiento = datetime.date(año, mes, dia)
+fecha_nacimiento = datetime.date(year, mes, dia)
 hoy = datetime.datetime.now()
 
-años = hoy.year - año
+years = hoy.year - fecha_nacimiento.year
 
-print('Tienes {} años. \n'.format(años))
+print('Tienes {} years. \n'.format(years))
 
 if fecha_nacimiento.month > hoy.month:
     print('Tu cumpleaños aún no ha pasado. \n')
 else:
     print('Tu cumpleaños ya paso! \n')
 
-if año >= 1920 and año <= 1939:
+if year >= 1920 and year <= 1939:
     print('Perteneces a la Generacion Silenciosa \n')
-elif año >= 1940 and año <= 1959:
+elif year >= 1940 and year <= 1959:
     print('Perteneces a los Baby Boomers \n')
-elif año >= 1960 and año <= 1979:
+elif year >= 1960 and year <= 1979:
     print('Perteneces a la Generacion X \n')
-elif año >= 1980 and año <= 1989:
+elif year >= 1980 and year <= 1989:
     print('Perteneces a la Generacion Y \n')
-elif año > 1990:
+else:
     print('Perteneces a la Generacion Z \n')
 
